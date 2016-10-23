bd08f6a4cfa94d8f630d41ef6ad80614a90a9c63
Lee-PC <604862524@qq.com>
2016-10-23 10:39:38 +0800

    通过response的write在后台打印信息到前台

---
+++ chap6/6-3.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class chap6_6_3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("<center>");
+        for (int i = 1; i <= 4; i++)
+        {
+            Response.Write("<p><font size="+i.ToString()+">我喜欢ASP.NET</font></p>");
+        }
+        Response.Write("</center>");
+    }
+}
