f5635b4b63b315535d4cd0c3296de091658c0290
Lee-PC <604862524@qq.com>
2016-10-23 10:03:09 +0800

    后台通过Request的ServerVariables和Browser对象获取一下服务器以及浏览器信息

---
+++ chap6/6-2.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class chap6_6_2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Text = "服务器IP地址：" + Request.ServerVariables["Local_ADDR"];
+        Label1.Text += "<br/>客户端IP地址：" + Request.ServerVariables["Remote_addr"];
+        Label1.Text += "<br/>浏览器类型：" + Request.Browser["Browser"];
+        Label1.Text += "<br/>浏览器版本：" + Request.Browser["Version"];
+        Label1.Text += "<br/>是否支持Cookies：" + Request.Browser["Cookies"];
+    }
+}
