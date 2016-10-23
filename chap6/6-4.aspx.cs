51b5f0a70b294d3848b577b4cebbe2563f1a6887
Lee-PC <604862524@qq.com>
2016-10-23 11:07:21 +0800

    通过response的redirect重定向网页

---
+++ chap6/6-4.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class chap6_6_4 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if ("teacher".Equals(RadioButtonList1.SelectedValue))
+            Response.Redirect("~/chap6/Teacher.aspx?name="+TextBox1.Text);
+    }
+}
