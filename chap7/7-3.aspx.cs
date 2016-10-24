50a7f4d2531af85a7025ad7885400fa4db77f6f7
Lee-PC <604862524@qq.com>
2016-10-24 16:23:21 +0800

    为数据源增设置属性达到增加删除，用databind实时更新数据

---
+++ chap7/7-3.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class chap7_7_3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlDataSource1.Insert();
+        GridView1.DataBind();
+    }
+}
