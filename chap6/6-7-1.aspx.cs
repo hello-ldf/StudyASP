1628549ff15923b6a661fff28ba290e709263f0e
Lee-PC <604862524@qq.com>
2016-10-24 08:05:32 +0800

    Request.Cookie.Add()保存cookie，Request.Cookie[]调用cookie

---
+++ chap6/6-7-1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class chap6_6_7_1 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (TextBox1.Text == "ssg" && TextBox2.Text == "123")
+        {
+            HttpCookie cookie = new HttpCookie("Name");
+            cookie.Value = TextBox1.Text;
+            cookie.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Add(cookie);
+            Response.Redirect("~/chap6/6-7-2.aspx");
+        }
+    }
+}
---
+++ chap6/6-7-2.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class chap6_6_7_2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Cookies["Name"] != null)
+        {
+            Label1.Text = Request.Cookies["Name"].Value + "，欢迎您回来！";
+        }
+        else
+        {
+            Response.Redirect("~/chap6/6-7-1.aspx");
+        }
+    }
+}
