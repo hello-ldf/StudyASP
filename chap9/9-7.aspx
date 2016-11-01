37d92ce822c4167bf2fd341bfaf2a01e91482817
Lee-notebook <604862524@qq.com>
2016-11-01 17:21:03 +0800

    把用户添加到角色 从角色中删除用户

---
+++ chap9/9-7.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="9-7.aspx.cs" Inherits="chap9_9_7" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="角色："></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="用户："></asp:Label>
+        </div>
+        <div style="margin-top: 5px">
+            <asp:ListBox ID="ListBox1" runat="server" Height="150px" Width="120px" AutoPostBack="true"></asp:ListBox>
+            &nbsp;&nbsp;
+            <asp:ListBox ID="ListBox2" runat="server" Height="150px" Width="120px" AutoPostBack="true" 
+                style="margin-left: 7px"></asp:ListBox>
+            <asp:Label ID="Label3" runat="server" Text=""></asp:Label>
+            <asp:Button ID="Button1" runat="server" Text="添加到角色" onclick="Button1_Click" />
+        </div>
+        <div style="margin-top: 21px">
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowDeleting="GridView1_RowDeleting">
+                <Columns>
+                    <asp:TemplateField HeaderText="用户名">
+                        <ItemTemplate>
+                            <asp:Label ID="Label1" runat="server" Text='<%# Container.DataItem.ToString() %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:CommandField DeleteText="从角色中删除" ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ chap9/9-7.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Web.Security;
+
+public partial class chap9_9_7 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string[] roles = Roles.GetAllRoles();
+            ListBox1.DataSource = roles;
+            ListBox1.DataBind();
+            MembershipUserCollection users = Membership.GetAllUsers();
+            ListBox2.DataSource = users;
+            ListBox2.DataBind();
+        }
+        if (ListBox1.SelectedItem != null)
+        {
+            GetUserInRoles();
+        }
+    }
+
+    private void GetUserInRoles()
+    {
+        string[] users = Roles.GetUsersInRole(ListBox1.SelectedValue);
+        GridView1.DataSource = users;
+        GridView1.DataBind();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (ListBox1.SelectedItem == null)
+        {
+            Label3.Text = "请选择角色";
+            return;
+        }
+        if (ListBox2.SelectedItem == null)
+        {
+            Label3.Text = "请选择用户";
+            return;
+        }
+
+        string[] users = new string[ListBox2.GetSelectedIndices().Length];
+        for (int i = 0; i < users.Length; i++)
+        {
+            users[i] = ListBox2.Items[ListBox2.GetSelectedIndices()[i]].Value;
+        }
+        try
+        {
+            Roles.AddUsersToRole(users, ListBox1.SelectedValue);
+            GetUserInRoles();
+        }
+        catch(Exception ee)
+        {
+            Label3.Text = ee.Message;
+        }
+    }
+
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        GridViewRow gvRow = GridView1.Rows[e.RowIndex];
+        Label userName = (Label)gvRow.Cells[0].FindControl("Label1");
+        try
+        {
+            Roles.RemoveUserFromRole(userName.Text, ListBox1.SelectedValue);
+        }
+        catch (Exception ee)
+        {
+            Label3.Text = ee.Message;
+        }
+        GetUserInRoles();
+    }
+}
