cec2753b4b71c135ae8485867b9be4c5dbe0d5ec
Lee-notebook <604862524@qq.com>
2016-11-03 15:04:12 +0800

    自定义控件，自定义事件 TagName控件标签名 TagPrefix标签前缀

---
+++ chap10/10-4.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="10-4.ascx.cs" Inherits="chap10_10_4" %>
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+<asp:Button ID="Button1" runat="server" Text="搜 索" onclick="Button1_Click" 
+    Width="146px" />
---
+++ chap10/10-4.ascx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class chap10_10_4 : System.Web.UI.UserControl
+{
+    private string _text;
+    public string Text
+    {
+        get { return _text; }
+        set { _text = value; }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Button1.Text = this.Text;
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text = "搜索完成";
+    }
+}
