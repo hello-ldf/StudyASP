bd65755a24c0d2261cdc9f050ef22e1a44e5cf33
Lee-PC <604862524@qq.com>
2016-11-13 00:33:03 +0800

    计算指定文件夹的大小 Directory类，DirectoryInfo类，File类，FileInfo类 System.IO

---
+++ chap14/14-2.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="14-2.aspx.cs" Inherits="chap14_14_2" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        输入目录：<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="计算目录大小" onclick="Button1_Click" />
+        <br /><br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        <br /><br />
+        <asp:TreeView ID="TreeView1" runat="server">
+        </asp:TreeView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ chap14/14-2.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+
+public partial class chap14_14_2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        TreeView1.Nodes.Clear();
+        string path = TextBox1.Text;
+        if (Directory.Exists(path))
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            TreeNode node = new TreeNode(path);
+            TreeView1.Nodes.Add(node);
+            Label1.Text = "目录大小：" + DirSize(node, dirInfo).ToString() + " Byte";
+        }
+        else
+        {
+            Label1.Text = "该路径不存在";
+        }
+    }
+
+    private static long DirSize(TreeNode parent, DirectoryInfo dirInfo)
+    {
+        long size = 0;
+
+        FileInfo[] files = dirInfo.GetFiles();
+        foreach (FileInfo file in files)
+        {
+            TreeNode childNode = new TreeNode();
+            childNode.Value = "文件：" + file.Name
+                + " 大小：" + file.Length
+                + " 日期：" + file.CreationTime;
+            parent.ChildNodes.Add(childNode);
+            size += file.Length;
+        }
+
+        DirectoryInfo[] dirs = dirInfo.GetDirectories();
+        foreach (DirectoryInfo dir in dirs)
+        {
+            TreeNode childNode = new TreeNode();
+            childNode.Value = "文件夹：" + dir.Name
+                + " 日期：" + dir.CreationTime;
+            parent.ChildNodes.Add(childNode);
+            size += DirSize(childNode, dir);
+        }
+
+        return size;
+    }
+}
