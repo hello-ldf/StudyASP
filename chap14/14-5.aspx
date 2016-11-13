b8c6d2f3d54f2226e15aa6fb1af32f383d2b3e6b
Lee-PC <604862524@qq.com>
2016-11-13 21:27:41 +0800

    StreamWriter和StreamReader类较FileStream类使用简单 写操作，每次写会直接覆盖之前的内容

---
+++ chap14/14-5.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="14-5.aspx.cs" Inherits="chap14_14_5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="写入" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="读取" OnClick="Button2_Click" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ chap14/14-5.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+
+public partial class chap14_14_5 : System.Web.UI.Page
+{
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string currentDir = Server.MapPath("");
+        string filePath = Path.Combine(currentDir, @"test.txt");
+        TextWriter sw = new StreamWriter(filePath);
+        sw.Write(TextBox1.Text);
+        sw.Flush();
+        sw.Close();
+        Label1.Text = "写入成功！";
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string currentDir = Server.MapPath("");
+        string filePath = Path.Combine(currentDir, @"test.txt");
+        TextReader sr = new StreamReader(filePath); // 如果文件不存在，报错
+        Label1.Text = sr.ReadToEnd();
+        sr.Close();
+    }
+}
