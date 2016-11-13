cc4e07dbbd63fcc0e114f426487401d76c21345a
Lee-PC <604862524@qq.com>
2016-11-13 19:20:22 +0800

    文件、文件夹的增删改查 Error：不支持给定路径的格式。 PS：File.Create(sourcePath); create之后返回一个fs，不关闭会报占用。

---
+++ chap14/14-3.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="14-3.aspx.cs" Inherits="chap14_14_3" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        源文件：<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="移动" onclick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="复制" onclick="Button2_Click" />&nbsp
+        目标文件：<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br /><br />
+        执行情况：<br />
+        <asp:Label ID="Label1" runat="server" Text="信息提示" BorderWidth="2" Font-Italic="true"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ chap14/14-3.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+
+public partial class chap14_14_3 : System.Web.UI.Page
+{
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string sourcePath = TextBox1.Text.Trim();
+        string targetPath = TextBox2.Text.Trim();
+        if (sourcePath.Length > 0 && targetPath.Length > 0)
+        {
+            Label1.Text = CopyMoveFile(sourcePath, targetPath, true);
+        }
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string sourcePath = TextBox1.Text.Trim();
+        string targetPath = TextBox2.Text.Trim();
+        if (sourcePath.Length > 0 && targetPath.Length > 0)
+        {
+            Label1.Text = CopyMoveFile(sourcePath, targetPath, false);
+        }
+    }
+
+    private string CopyMoveFile(string sourcePath, string targetPath, Boolean flag)
+    {
+        string msg = "";
+
+        //string pathRoot = Server.MapPath("");
+        //sourcePath = Path.Combine(pathRoot, sourcePath);
+        //targetPath = Path.Combine(pathRoot, targetPath);
+
+        try
+        {
+            string dirName = Path.GetDirectoryName(sourcePath);
+            if (!Directory.Exists(dirName))
+            {
+                Directory.CreateDirectory(dirName);
+                msg += "1.源文件夹不存在，新建源文件夹。<br/>";
+            }
+
+            // string fileName = Path.GetFileName(sourcePath);
+            if (!File.Exists(sourcePath))
+            {
+                using (FileStream fs = File.Create(sourcePath)) { }
+                //File.Create(sourcePath);  create之后返回一个fs，不关闭会报占用。
+                msg += "2.源文件不存在，新建源文件。<br/>";
+            }
+
+            string targetDirName = Path.GetDirectoryName(targetPath);
+            if (!Directory.Exists(targetDirName))
+            {
+                Directory.CreateDirectory(targetDirName);
+                msg += "3.目标文件夹不存在，新建目标文件夹。<br/>";
+            }
+
+            if (flag)
+            {
+                if (File.Exists(targetPath))
+                {
+                    File.Delete(targetPath);
+                    msg += "4.目标文件已存在，删除目标文件。<br/>";
+                }
+                File.Move(sourcePath, targetPath);
+                msg += "5-2.移动文件。<br/>";
+            }
+            else
+            {
+                if (File.Exists(targetPath))
+                {
+                    File.Delete(targetPath);
+                    msg += "4.目标文件已存在，删除目标文件。<br/>";
+                }
+                File.Copy(sourcePath, targetPath);
+                msg += "5-1.复制文件。<br/>";
+            }
+
+            if (File.Exists(sourcePath))
+            {
+                msg += "6-1.源文件存在，复制完成。<br/>";
+            }
+            else
+            {
+                msg += "6-2.源文件不存在，移动完成。<br/>";
+            }
+        }
+        catch (Exception e)
+        {
+            msg += "7.异常。" + e.Message;
+        }
+
+        return msg;
+    }
+}
