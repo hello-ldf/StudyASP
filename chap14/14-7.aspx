a18321971fd938b3757dff14e32739955e438567
Lee-PC <604862524@qq.com>
2016-11-13 22:18:56 +0800

    FileUpload控件实现文件上传

---
+++ chap14/14-7.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="14-7.aspx.cs" Inherits="chap14_14_7" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="提交" OnClick="Button1_Click" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ chap14/14-7.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class chap14_14_7 : System.Web.UI.Page
+{
+    private string fileDir;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string rootPath = Request.PhysicalApplicationPath;
+        fileDir = Path.Combine(rootPath, "UpLoads");
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.PostedFile.FileName.Trim().Length == 0)
+        {
+            Label1.Text = "无文件上传";
+            return;
+        }
+
+        if (FileUpload1.PostedFile.ContentLength > 204800)
+        {
+            Label1.Text = "文件大于200KB，上传失败";
+            return;
+        }
+
+        string extension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+        if (extension != ".bmp" && extension != ".gif" && extension != ".jpg")
+        {
+            Label1.Text = "文件类型不是.bmp .gif .jpg";
+            return;
+        }
+
+        try
+        {
+            string filePath = Path.Combine(fileDir, FileUpload1.PostedFile.FileName);
+            FileUpload1.PostedFile.SaveAs(filePath);
+            Label1.Text = "文件上传成功";
+        }
+        catch(Exception exc)
+        {
+            Label1.Text = "文件上传失败。" + exc.Message;
+        }
+    }
+}
