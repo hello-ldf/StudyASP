18a9dd40065bdaca53620f2e60a9736434a49776
Lee-PC <604862524@qq.com>
2016-11-13 21:00:54 +0800

    用FileStream类读写文件 Encoding类需要using System.Text

---
+++ chap14/14-4.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="14-4.aspx.cs" Inherits="chap14_14_4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="添加" OnClick="Button1_Click" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ chap14/14-4.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.Text;
+
+public partial class chap14_14_4 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string filePath = Path.Combine(Request.PhysicalApplicationPath, @"chap14/test.txt");
+        if (File.Exists(filePath))
+        {// 打开 读
+            Label1.Text = ReadFile(filePath);
+        }
+        else
+        {// 新建 写
+            AppendFile("The First Line!");
+            Label1.Text = "The First Line!";
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        AppendFile(TextBox1.Text);
+        Label1.Text += TextBox1.Text;
+    }
+    private void AppendFile(string str)
+    {
+        string filePath = Path.Combine(Request.PhysicalApplicationPath, @"chap14/test.txt");
+        FileStream fs = File.Open(filePath, FileMode.Append, FileAccess.Write, FileShare.None);
+        byte[] dataBuf = new byte[str.Length];
+        dataBuf = Encoding.UTF8.GetBytes(str);
+        fs.Write(dataBuf, 0, str.Length);
+        fs.Flush();
+        fs.Close();
+    }
+    private string ReadFile(string filePath)
+    {
+        FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        byte[] data = new byte[fs.Length];
+        fs.Read(data, 0, (int)fs.Length);
+        fs.Close();
+        return Encoding.UTF8.GetString(data);
+    }
+
+}
